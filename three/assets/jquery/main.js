471972a888dac6c66d46494d272f0f8da5971155
racoon12345679 <zoexieyunyi@gmail.com>
2017-11-03 00:58:31 +1100

    three assests

--- three/assets/jquery/main.js
+++ three/assets/jquery/main.js
@@ -16,6 +16,19 @@ $(document).ready(function() {
     $('.title').hide();
 });
 
+/*caption*/
+
+$(document).ready(function() {
+  $('.content').mouseenter(function() {
+    $(this).find('span').slideToggle();
+  });
+
+  $('.content').mouseleave(function() {
+    $(this).find('span').slideToggle();
+  });
+});
+
+
 
 /*fade*/
 
@@ -23,7 +36,7 @@ $(document).ready(function() {
     $('body').removeClass('fade-out');
 });
 
-/*hover title*/
+/*hover title
 $('img.thumb').hover(
     function(){
         $(this).css('opacity','.5');
@@ -32,6 +45,27 @@ $('img.thumb').hover(
         $(this).css('opacity','1');
     }
 );
+*/
 
+/* Every time the window is scrolled ...
+  $(window).scroll( function(){
+
+      /* Check the location of each desired element
+      $('.content').each( function(i){
+
+          var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+          var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+          /* If the object is completely visible in the window, fade it it
+          if( bottom_of_window > bottom_of_object ){
+
+              $(this).animate({'opacity':'1'},500);
+
+          }
+
+      });
+
+  });
+*/
 
 });
